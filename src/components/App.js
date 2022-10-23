90deb5a7642e578480f70e1e93fc67d3f1f15b49
miinii <ft.dawn.o@gmail.com>
2022-10-24 00:17:46 +0900

    :wrench: Feat : Firebase Config Setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactDOM } from 'react';
 import AppRouter from './Router';
+import { authService } from '../fBase';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  // NOTE firebase 공식문서를 보면 currentUser의 기본값은 null
+  console.log('🚀 ⁝ App ⁝ authService.currentUser', authService.currentUser);
+
   return (
     <>
-      <   AppRouter />
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Ntwitter{new Date().getFullYear()}</footer>
     </>
   );
 };
--- src/components/Router.js
+++ src/components/Router.js
@@ -5,17 +5,16 @@ import { HashRouter as BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(isLoggedIn);
-
+const AppRouter = ({ isLoggedIn }) => {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={isLoggedIn? (<Home />): <Auth/>} />
+        <Route path='/' element={isLoggedIn ? <Home /> : <Auth />} />
       </Routes>
     </BrowserRouter>
   );
-};      
+};
 
 export default AppRouter;
